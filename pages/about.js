e664831b39e0e497e185c9403c25cd208b699e9f
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-23 09:54:43 +0200

    Add about text

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,6 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import Wrapper from "../components/Wrapper";
 import Main from "../components/Main";
-import Section from "../components/Section";
 
 export default function About() {
   return (
@@ -16,12 +15,26 @@ export default function About() {
       <NavBar />
 
       <Main>
-        <Section>
+        <header>
           <h1>About me</h1>
-        </Section>
+        </header>
+        <article>
+          <p>What does a $2 computer at a flea market have to do with me writing this?</p>
+          <p>In my case, turns out, everything! </p>
+          <p>
+            Proin ligula ipsum, rutrum ut tellus in, venenatis vestibulum elit. Donec a sagittis
+            augue. Nullam at molestie eros. Aliquam quis neque varius, facilisis erat sagittis,
+            cursus lacus. Curabitur nec libero turpis. Sed ut nibh justo. Sed eu dictum ex. Proin
+            semper a tellus non hendrerit. Mauris malesuada pellentesque lacinia.
+          </p>
+        </article>
       </Main>
 
       <Footer />
     </Wrapper>
   );
 }
+
+const Header = styled.header`
+  margin-bottom: 32px;
+`;
