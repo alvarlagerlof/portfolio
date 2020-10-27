c48448c91156330b45c2fc0021cb043f0ed059f5
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-27 21:56:42 +0100

    Added twitter:image

--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,7 @@ export default function Home({ image, posts, projects }) {
           <meta property="og:image" content={image}></meta>
           <meta name="twitter:card" content="summary_large_image"></meta>
           <meta name="twitter:creator" content="@alvarlagerlof"></meta>
+          <meta name="twitter:image" content={image}></meta>
         </Head>
 
         <NavBar />
