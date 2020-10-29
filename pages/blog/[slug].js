6dbdc903f9f336b86e03206d0458feb7d8d7fcf7
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-29 15:53:53 +0100

    Move markdown renderer

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -7,7 +7,7 @@ import branchName from "current-git-branch";
 import { formatDate } from "../../libs/utils/date";
 import { getPosts, getPost } from "../../libs/blog";
 import { getImage } from "../../libs/image";
-import { renderers } from "./markdownRenderers";
+import { renderers } from "../../components/markdownRenderers";
 
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
