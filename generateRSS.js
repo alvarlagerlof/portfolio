1816875ed890b7f9bc9f410b5049bc454c3dd689
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-30 11:21:18 +0100

    With content and reference

--- generateRSS.js
+++ generateRSS.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const RSS = require("rss");
 const matter = require("gray-matter");
+const md = require("markdown-it")();
 
 try {
   const posts = fs
@@ -8,32 +9,36 @@ try {
     .map(filename => {
       const markdownWithMetadata = fs.readFileSync(`content/blog/${filename}`).toString();
 
-      const { data } = matter(markdownWithMetadata);
+      const { data, content } = matter(markdownWithMetadata);
 
       return {
         ...data,
+        content,
         slug: filename.replace(".md", ""),
         date: data.date,
       };
     })
-    .filter(post => post.draft == false)
+    .filter(post => post.draft == true)
     .sort((a, b) => b.date - a.date);
 
   const feed = new RSS({
     title: "Alvar Lagerlöf's Blog",
-    site_url: "https://alvar.dev",
-    feed_url: "https://alvar.dev/feed.xml",
+    link: "https://alvar.dev",
     language: "en-us",
+    description: "Developer and designer living in Stockholm",
   });
 
   posts.forEach(post => {
+    const link = `<br/>(This is an article posted to my blog at alvar.dev. You can read it online by <a href="https://alvar.dev/blog/${post.slug}">clicking here</a>.)`;
+    const content = md.render(post.content) + link;
+
     feed.item({
       title: post.title,
-      guid: post.slug,
       url: `https://alvar.dev/blog/${post.slug}`,
       date: post.date,
       description: post.description,
       author: "Alvar Lagerlöf",
+      custom_elements: [{ "content:encoded": content }],
     });
   });
 
