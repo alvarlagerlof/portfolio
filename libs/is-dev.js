16a1991e4e1a1cd1d5c267cb050274ad839af706
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-30 13:01:54 +0100

    Logging

--- libs/is-dev.js
+++ libs/is-dev.js
@@ -2,8 +2,12 @@ import process from "process";
 import branchName from "current-git-branch";
 
 export default function isDev() {
+  console.log("ref", process.env.VERCEL_GITHUB_COMMIT_REF);
+  console.log("brancName()", branchName());
+
   return (
-    (process.env.VERCEL_GITHUB_COMMIT_REF && process.env.VERCEL_GITHUB_COMMIT_REF != "main") ||
-    (branchName() && branchName() != "main")
+    (process.env.VERCEL_GITHUB_COMMIT_REF != undefined &&
+      process.env.VERCEL_GITHUB_COMMIT_REF != "main") ||
+    (branchName() != false && branchName() != "main")
   );
 }
