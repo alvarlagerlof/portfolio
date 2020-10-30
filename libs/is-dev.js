5c0db144bd5f3348d0d942aedc2d7136f984a31f
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-30 12:26:36 +0100

    Improve is-dev

--- libs/is-dev.js
+++ libs/is-dev.js
@@ -2,5 +2,8 @@ import process from "process";
 import branchName from "current-git-branch";
 
 export default function isDev() {
-  return process.env.VERCEL_GITHUB_COMMIT_REF != "main" || branchName != "main";
+  return (
+    (process.env.VERCEL_GITHUB_COMMIT_REF && process.env.VERCEL_GITHUB_COMMIT_REF != "main") ||
+    (branchName() && branchName() != "main")
+  );
 }
