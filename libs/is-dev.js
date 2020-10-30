5103ff31740ffaf497f36b81022bdfdadf4a51db
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-30 13:37:04 +0100

    Comment

--- libs/is-dev.js
+++ libs/is-dev.js
@@ -2,6 +2,8 @@ import process from "process";
 import branchName from "current-git-branch";
 
 export default function isDev() {
+  // branchName() returns "master" every time in Vercel
+
   return (
     process.env.IS_DEV == "true" ||
     (process.env.NODE_ENV == "development" && branchName() != "main")
