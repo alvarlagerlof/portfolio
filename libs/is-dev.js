9b76c175f8e4476e5a353922d03ec0d9c5eca0ca
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-30 13:22:18 +0100

    Proper logging

--- libs/is-dev.js
+++ libs/is-dev.js
@@ -2,7 +2,11 @@ import process from "process";
 import branchName from "current-git-branch";
 
 export default function isDev() {
+  console.log("process.env.IS_DEV", process.env.IS_DEV);
+  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+  console.log("branchName()", branchName());
+
   return (
-    process.env.IS_DEV != "main" || (process.env.NODE_ENV != "production" && branchName() != "main")
+    process.env.IS_DEV == true || (process.env.NODE_ENV == "development" && branchName() != "main")
   );
 }
