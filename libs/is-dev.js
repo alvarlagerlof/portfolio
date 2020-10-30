39f7138bff8578b88155a25b1374887c502a60a3
Alvar Lagerlöf <alvar.lagerlof@gmail.com>
2020-10-30 13:25:40 +0100

    Remove logging

--- libs/is-dev.js
+++ libs/is-dev.js
@@ -2,12 +2,6 @@ import process from "process";
 import branchName from "current-git-branch";
 
 export default function isDev() {
-  console.log("process.env.IS_DEV", process.env.IS_DEV);
-  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
-  console.log("branchName()", branchName());
-
-  console.log("test", process.env.IS_DEV == "true");
-
   return (
     process.env.IS_DEV == "true" ||
     (process.env.NODE_ENV == "development" && branchName() != "main")
