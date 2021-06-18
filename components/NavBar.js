814d81322a1fd42971386235d57bb5b91f1c4542
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-06-18 12:08:05 +0200

    Svg conversion to react

--- components/NavBar.js
+++ components/NavBar.js
@@ -56,7 +56,7 @@ function IconProjects({ active }) {
     <svg
       aria-hidden
       xmlns="http://www.w3.org/2000/svg"
-      enable-background="new 0 0 24 24"
+      enableBackground="new 0 0 24 24"
       height="24px"
       viewBox="0 0 24 24"
       width="24px"
