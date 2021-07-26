a3ad0b4df410a536d9058919c6a80a9963b33f7c
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-27 01:11:15 +0200

    Show update date

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -38,9 +38,13 @@ function Header({ post }: PostProps) {
       {post.draft && (
         <p className=" bg-primary px-2 rounded-full inline-block text-white h-6 mb-8">Draft</p>
       )}
-      <p className="font-medium md:text-xl mb-2">{formatDate(post.date.published)}</p>
+
       <h1 className="font-heading text-4xl md:text-7xl mb-8 ">{post.title}</h1>
-      <h2 className="font-subheading text-xl md:text-2xl max-w-[60ch]">{post.description}</h2>
+      <h2 className="font-subheading text-xl md:text-2xl max-w-[60ch] mb-8">{post.description}</h2>
+      <p className="font-medium">
+        Published {formatDate(post.date.published)}
+        {post.date.updated && ` - Updated ${formatDate(post.date.updated)}`}
+      </p>
     </header>
   );
 }
