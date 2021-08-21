1d54954530086cec4a8da47b4d0fc9b2d4311477
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-08-22 00:10:24 +0200

    Twitter link in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,11 @@
+import ArrowLink from "./ArrowLink";
+
 export default function Footer() {
   return (
     <footer className="mb-8 md:mb-14">
       <p className="font-subheading xl:text-xl">
-        Made with <span className="text-primary">❤</span> in Stockholm
+        Made with <span className="text-primary">❤</span> in Stockholm by{" "}
+        <ArrowLink href="https://twitter.com/alvarlagerlof"> @alvarlagerlof</ArrowLink>
       </p>
     </footer>
   );
