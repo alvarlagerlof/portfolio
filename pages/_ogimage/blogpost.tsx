5f99f84e36b3cc76fe4f669045b5321595b3e69a
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-08-25 00:19:53 +0200

    Line height in og:image

--- pages/_ogimage/blogpost.tsx
+++ pages/_ogimage/blogpost.tsx
@@ -13,7 +13,7 @@ export default function Default() {
     <div className="w-[1200px] h-[630px] fixed top-0 left-0 bg-green-100 py-20 px-24 flex flex-col justify-between">
       <div className="space-y-2">
         <h1 className="font-heading text-[5.7em] leading-[1.2]">{title}</h1>
-        <h2 className="font-subheading text-[2.7em]">{description}</h2>
+        <h2 className="font-subheading text-[2.7em] leading-[1.4]">{description}</h2>
       </div>
 
       <div className="flex flex-row justify-between">
