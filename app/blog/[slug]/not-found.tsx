b6972b2affb7d145a284df9d9ad642ac7b6c0111
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-10-30 02:24:13 +0200

    Specific 404 for blog posts

--- app/blog/[slug]/not-found.tsx
+++ app/blog/[slug]/not-found.tsx
@@ -1,12 +1,12 @@
 import ArrowLink from "components/ArrowLink";
 
-export default function NotFoundPage() {
+export default function BlogPostFoundNotPage() {
   return (
     <header>
       <h1 className="font-heading text-7xl mb-4">404</h1>
 
       <h2 className="font-subheading text-2xl">
-        Page not found. Please go <ArrowLink href="/">home</ArrowLink>
+        Blog post not found. You can find all blog posts <ArrowLink href="/">here</ArrowLink>
       </h2>
     </header>
   );
