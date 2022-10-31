0564f4689a5058069dc9b881883d2a253ffb5d81
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-10-31 09:18:54 +0100

    Simper footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,6 @@ export default function Footer() {
   return (
     <footer className="mb-8 md:mb-14">
       <p className="font-subheading xl:text-xl">
-        Made with <span className="text-primary">❤</span> by{" "}
         <ArrowLink href="https://twitter.com/alvarlagerlof">@alvarlagerlof</ArrowLink>
       </p>
     </footer>
