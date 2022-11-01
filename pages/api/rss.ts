c2831f23f4161b299b15940a17eeb62a6409d806
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-11-02 00:05:07 +0100

    Improve rss and and make it dynamic

---
+++ pages/api/rss.ts
@@ -0,0 +1,79 @@
+import { toHTML } from "@portabletext/to-html";
+import htm from "htm";
+import vhtml from "vhtml";
+
+import { Feed } from "feed";
+
+import { Post } from "types";
+import { getClient } from "lib/sanity/sanity.server";
+import { groq } from "next-sanity";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const html = htm.bind(vhtml);
+
+const query = groq`
+*[_type == "post"] | order(date.published desc) {
+  _id,
+  slug,
+  title,
+  description,
+  date,
+  body
+}
+`;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const posts: Post[] = await getClient().fetch(query);
+
+    const feed = new Feed({
+      title: "Alvar Lagerlöf's Blog",
+      id: "https://alvar.dev",
+      link: "https://alvar.dev",
+      copyright: "",
+      language: "en-us",
+      favicon: "http://alvar.dev/favicon.ico",
+      description: "Developer and designer living in Stockholm",
+      author: {
+        name: "Alvar Lagerlöf",
+        email: "hello@alvar.dev",
+        link: "https://alvar.dev",
+      },
+    });
+
+    posts.forEach(post => {
+      const link = `<br/>(This is an article posted to my blog at alvar.dev. You can read it online by <a href="https://alvar.dev/blog/${post.slug.current}">clicking here</a>.)`;
+      const content =
+        toHTML(post.body, {
+          components: {
+            types: {
+              image: ({ value }) => String(html`<img src="${value.imageUrl}" />`),
+            },
+          },
+        }) + link;
+
+      feed.addItem({
+        title: post.title,
+        id: `https://alvar.dev/blog/${post.slug.current}`,
+        link: `https://alvar.dev/blog/${post.slug.current}`,
+        description: post.description,
+        author: [
+          {
+            name: "Alvar Lagerlöf",
+            email: "hello@alvar.dev",
+            link: "https://alvar.dev",
+          },
+        ],
+        content: content,
+        date: new Date(post.date.published),
+      });
+    });
+
+    const rss = feed.rss2();
+
+    res.status(200).send(rss);
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    res.status(500).send({ error: "failed generate feed" });
+  }
+}
