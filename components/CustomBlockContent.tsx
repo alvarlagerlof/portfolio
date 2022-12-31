507e505538f21808fbc0a865eeb57f17d7a9f2d5
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-12-31 19:45:17 +0100

    Fix block content hydration error and refactor structure
    The `code` mark was rendering `pre` inside `p`

--- components/CustomBlockContent.tsx
+++ components/CustomBlockContent.tsx
@@ -6,75 +6,80 @@ import { CodeBlock } from "./CodeBlock";
 import { NextSanityImage } from "./SanityImage";
 import { Skeleton } from "./Skeleton";
 
-const components: Partial<PortableTextReactComponents> = {
-  marks: {
-    link: ({ value, children }) => {
-      const { href } = value;
-
-      return (
-        <ArrowLink newTab href={href}>
-          {children}
-        </ArrowLink>
-      );
-    },
-
-    internalLink: ({ value, children }) => {
-      const {
-        slug: { current },
-      } = value;
-      const href = `/blog/${current}`;
-
-      return <ArrowLink href={href}>{children}</ArrowLink>;
-    },
-
-    code: ({ children }) => {
-      return (
-        <pre className="inline-block px-1 -py-2 bg-[rgb(227,233,242)] whitespace-normal">
-          {children}
-        </pre>
-      );
-    },
-  },
-
-  types: {
-    code(props) {
-      const { language, code } = props.value;
-
-      return (
-        <Suspense fallback={<Skeleton className="w-full h-96" />}>
-          <CodeBlock language={language} code={code} />
-        </Suspense>
-      );
-    },
-
-    image({ value }) {
-      return (
-        <NextSanityImage
-          image={value}
-          className="rounded-3xl border-2 border-imgborder"
-          alt={value.caption}
-        />
-      );
-    },
-  },
-
-  block: {
-    h1: ({ children }) => {
-      return <h3 className="font-subheading font-semibold !text-2xl !md:text-4xl">{children}</h3>;
-    },
-
-    h2: ({ children }) => {
-      return <h4 className="font-subheading font-semibold !text-xl !md:text-3xl">{children}</h4>;
-    },
-
-    blockquote: ({ children }) => {
-      return (
-        <blockquote className="py-2 px-4  !border-separator flex flex-col">{children}</blockquote>
-      );
-    },
-  },
-};
-
 export function CustomBlockContent({ blocks }) {
-  return <PortableText value={blocks} components={components} />;
+  return (
+    <PortableText
+      value={blocks}
+      components={{
+        marks: {
+          link: ({ value, children }) => {
+            const { href } = value;
+
+            return (
+              <ArrowLink newTab href={href}>
+                {children}
+              </ArrowLink>
+            );
+          },
+
+          internalLink: ({ value, children }) => {
+            const {
+              slug: { current },
+            } = value;
+            const href = `/blog/${current}`;
+
+            return <ArrowLink href={href}>{children}</ArrowLink>;
+          },
+
+          code: ({ children }) => {
+            return (
+              <code className="inline-block px-1 -py-2 bg-[rgb(227,233,242)] whitespace-normal">
+                {children}
+              </code>
+            );
+          },
+        },
+
+        types: {
+          code(props) {
+            const { language, code } = props.value;
+            return (
+              <Suspense fallback={<Skeleton className="w-full h-96" />}>
+                <CodeBlock language={language} code={code} />
+              </Suspense>
+            );
+          },
+          image({ value }) {
+            return (
+              <NextSanityImage
+                image={value}
+                className="rounded-3xl border-2 border-imgborder"
+                alt={value.caption}
+              />
+            );
+          },
+        },
+
+        block: {
+          h1: ({ children }) => {
+            return (
+              <h3 className="font-subheading font-semibold !text-2xl !md:text-4xl">{children}</h3>
+            );
+          },
+          h2: ({ children }) => {
+            return (
+              <h4 className="font-subheading font-semibold !text-xl !md:text-3xl">{children}</h4>
+            );
+          },
+          blockquote: ({ children }) => {
+            return (
+              <blockquote className="py-2 px-4  !border-separator flex flex-col">
+                {children}
+              </blockquote>
+            );
+          },
+        },
+      }}
+    />
+  );
 }
