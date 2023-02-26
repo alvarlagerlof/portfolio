20db52c5096db81563dbfdba5f31a9f81c062448
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-02-26 01:52:32 +0100

    Switch code renderer to bright

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,20 +1,10 @@
-import Syntax from "react-syntax-highlighter/dist/esm/default-highlight";
-import style from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-plateau-light";
+import { Code } from "bright";
 
-export function CodeBlock({ language, code }) {
+export function CodeBlock({ language, code }: { language: string; code: string }) {
   return (
-    <Syntax
-      customStyle={{
-        borderRadius: "0px",
-        padding: "16px",
-        fontFamily: "unset",
-        fontSize: "0.9em",
-        width: "100%",
-      }}
-      style={style}
-      language={language}
-    >
-      {code}
-    </Syntax>
+    // @ts-ignore
+    <Code lang={language} theme="dracula-soft">
+      {code.trim()}
+    </Code>
   );
 }
