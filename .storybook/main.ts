cd67919e6ec9e2730654ebadd47feccf6a3059b8
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-04-10 20:01:36 +0200

    test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -13,5 +14,16 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  // previewHead(head, options) {
+  //   console.log("TETE", JSON.stringify(head), JSON.stringify(options))
+  //   return `
+  //   <style>tja</style/>${head}
+  //   <style>
+  //     #main {
+  //       background-color: yellow;
+  //     }
+  //   </style>
+  // `
+  // },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,36 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+import { Preview } from '@storybook/react'
+
+async function withStyles(context) {
+  const { parameters: { stylesPath } } = context;
+  // Asynchronously import styles and append them to `<head>`
+
+  console.log("before")
+  await new Promise(resolve => setTimeout(resolve, 30));
+
+  var style = document.createElement('style');
+  style.id = 'myFavicon';
+  style.textContent = `button { background: red !important; }`
+
+  document.head.appendChild(style)
+  console.log("after")
+
+  console.log(context)
+
+
+  return {}
 }
+
+
+const preview: Preview = {
+  loaders: [withStyles],
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  }
+}
+export default preview
