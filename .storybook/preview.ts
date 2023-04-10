56cb5c999f691dc78bc5872ca8b33e47a038ce92
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-04-10 20:21:26 +0200

    testing

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,26 +1,51 @@
 import { Preview } from '@storybook/react'
 
-async function withStyles(context) {
-  const { parameters: { stylesPath } } = context;
-  // Asynchronously import styles and append them to `<head>`
-
-  console.log("before")
+async function loadStyle1() {
   await new Promise(resolve => setTimeout(resolve, 30));
+  return `button { background: red !important; }`
+}
 
+async function loadStyle2() {
+  await new Promise(resolve => setTimeout(resolve, 30));
+  return `button { background: green !important; }`
+}
+
+
+async function loadStyle3() {
+  await new Promise(resolve => setTimeout(resolve, 30));
+  return `button { padding: 20px !important; }`
+}
+
+async function withStyles(context) {
+  const { title } = context;
+
+  // console.log("before")
+
+  let finalStyles = ""
+
+  if (title.startsWith("Example")) {
+    finalStyles += await loadStyle1()
+  }
+
+  if (title.startsWith("Testing")) {
+    finalStyles += await loadStyle2()
+    finalStyles += await loadStyle3()
+  }
+
+  console.log({finalStyles})
 
   const existingStyleTag = document.querySelector('[data-name="customStyle"]')
 
   if (existingStyleTag) {
-    existingStyleTag.textContent = `button { background: green !important; }`
+    existingStyleTag.textContent = finalStyles
   } else {
     var style = document.createElement('style');
-    style.textContent = `button { background: red !important; }`
+    style.textContent = finalStyles
     style.dataset.name = "customStyle"
     document.head.appendChild(style)
   }
 
-  console.log("after")
-  console.log(context)
+  // console.log("after")
 
 
   return {}
