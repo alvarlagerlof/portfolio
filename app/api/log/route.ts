fef092cf45c67f5b02eb6db4b7720463c240dcda
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-06-03 20:33:42 +0200

    Insert into Clickhouse

--- app/api/log/route.ts
+++ app/api/log/route.ts
@@ -2,7 +2,11 @@
 // const { json } = require("micro");
 
 export async function POST(request: Request) {
-  if (!process.env.LOG_DRAIN_VERCEL_VERIFY) {
+  if (
+    !process.env.CLICKHOUSE_USER ||
+    !process.env.CLICKHOUSE_PASSWORD ||
+    !process.env.LOG_DRAIN_VERCEL_VERIFY
+  ) {
     return new Response(null, {
       status: 500,
     });
@@ -25,17 +29,41 @@ export async function POST(request: Request) {
   //     });
   //   }
 
-  console.log(await request.text());
+  const text = await request.text();
+  console.log(text);
 
-  //   await fetch("http://localhost:8123/?user=user&password=password", {
-  //     method: "POST",
-  //     body: `INSERT INTO t VALUES (1),(2),(3)`,
-  //   });
+  try {
+    await fetch(
+      `http://localhost:8123/?user=${process.env.CLICKHOUSE_USER}&password=${process.env.CLICKHOUSE_PASSWORD}`,
+      {
+        method: "POST",
+        body: `INSERT INTO vercel_log FORMAT JSONEachRow
+    ${text}`,
+      }
+    );
 
-  return new Response(null, {
-    status: 200,
-    headers: {
-      "x-vercel-verify": process.env.LOG_DRAIN_VERCEL_VERIFY,
-    },
-  });
+    return new Response(null, {
+      status: 200,
+      headers: {
+        "x-vercel-verify": process.env.LOG_DRAIN_VERCEL_VERIFY,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+        return new Response(error.message, {
+            status: 500,
+            headers: {
+              "x-vercel-verify": process.env.LOG_DRAIN_VERCEL_VERIFY,
+            },
+          });
+        }
+    }
+    return new Response("Server error", {
+        status: 500,
+        headers: {
+          "x-vercel-verify": process.env.LOG_DRAIN_VERCEL_VERIFY,
+        },
+      });
+    }
+
 }
