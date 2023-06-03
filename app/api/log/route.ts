5d17cb10bd45920f9908886c8906aa2d6adc29e3
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-06-03 21:40:25 +0200

    Show longer preview

--- app/api/log/route.ts
+++ app/api/log/route.ts
@@ -37,7 +37,7 @@ export async function POST(request: Request) {
 
     console.log(
       "PREVIEW",
-      `${sql}\n${lines.map(line => `{"event":${line.substring(0, 30)}}`).join("\n")}`
+      `${sql}\n${lines.map(line => `{"event":${line.substring(0, 150)}}`).join("\n")}`
     );
 
     const body = `${sql}\n${lines.map(line => `{"event":${line}}`).join("\n")}`;
