482042ce2870c83d3821fcc2a0ff346150b9d29d
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-06-03 18:48:12 +0200

    Remove verification for now

--- app/api/log/route.ts
+++ app/api/log/route.ts
@@ -1,30 +1,11 @@
-import crypto from "crypto";
-import arrayBufferToBuffer from "arraybuffer-to-buffer";
-
 export async function GET(request: Request) {
-  if (!process.env.INTEGRATION_SECRET || !process.env.LOG_DRAIN_VERCEL_VERIFY) {
+  if (!process.env.LOG_DRAIN_VERCEL_VERIFY) {
     return new Response(null, {
       status: 500,
     });
   }
 
-  const rawBody = await request.arrayBuffer();
-  const rawBodyBuffer = arrayBufferToBuffer(rawBody);
-  const bodySignature = await sha1(rawBodyBuffer, process.env.INTEGRATION_SECRET);
-
-  if (bodySignature !== request.headers["x-vercel-signature"]) {
-    return new Response(
-      JSON.stringify({
-        code: "invalid_signature",
-        error: "signature didn't match",
-      }),
-      {
-        status: 403,
-      }
-    );
-  }
-
-  console.log(rawBody);
+  console.log(await request.json());
 
   return new Response(null, {
     status: 200,
@@ -32,8 +13,4 @@ export async function GET(request: Request) {
       "x-vercel-verify": process.env.LOG_DRAIN_VERCEL_VERIFY,
     },
   });
-}
-
-async function sha1(data: Buffer, secret: string) {
-  return crypto.createHmac("sha1", secret).update(data).digest("hex");
 }
