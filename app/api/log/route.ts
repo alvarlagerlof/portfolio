a3d6fc029f3ae430521c81f2e7bdb0961c482ac9
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-06-03 19:18:47 +0200

    Verify body

--- app/api/log/route.ts
+++ app/api/log/route.ts
@@ -1,7 +1,21 @@
+const crypto = require("crypto");
+const { json } = require("micro");
+
 export async function POST(request: Request) {
-  if (!process.env.LOG_DRAIN_VERCEL_VERIFY) {
+  if (!process.env.LOG_DRAIN_VERCEL_VERIFY || !process.env.LOG_DRAIN_SECRET) {
     return new Response(null, {
       status: 500,
+    });
+  }
+
+  const payload = await json(request);
+  const signature = crypto
+    .createHmac("sha1", process.env.LOG_DRAIN_SECRET)
+    .update(payload)
+    .digest("hex");
+  if (signature !== request.headers["x-vercel-signature"]) {
+    return new Response("Unauthorized", {
+      status: 403,
     });
   }
 
