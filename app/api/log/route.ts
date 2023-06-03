5c5fcc6b693303dd8ed0d65cf0f128e4a5df6fdf
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-06-03 19:44:37 +0200

    Disable verification again

--- app/api/log/route.ts
+++ app/api/log/route.ts
@@ -1,23 +1,29 @@
-const crypto = require("crypto");
-const { json } = require("micro");
+// const crypto = require("crypto");
+// const { json } = require("micro");
 
 export async function POST(request: Request) {
-  if (!process.env.LOG_DRAIN_VERCEL_VERIFY || !process.env.LOG_DRAIN_SECRET) {
+  if (!process.env.LOG_DRAIN_VERCEL_VERIFY) {
     return new Response(null, {
       status: 500,
     });
   }
 
-  const payload = await json(request);
-  const signature = crypto
-    .createHmac("sha1", process.env.LOG_DRAIN_SECRET)
-    .update(payload)
-    .digest("hex");
-  if (signature !== request.headers["x-vercel-signature"]) {
-    return new Response("Unauthorized", {
-      status: 403,
-    });
-  }
+  //   if (!process.env.LOG_DRAIN_SECRET) {
+  //     return new Response(null, {
+  //       status: 500,
+  //     });
+  //   }
+
+  //   const payload = await json(request);
+  //   const signature = crypto
+  //     .createHmac("sha1", process.env.LOG_DRAIN_SECRET)
+  //     .update(payload)
+  //     .digest("hex");
+  //   if (signature !== request.headers["x-vercel-signature"]) {
+  //     return new Response("Unauthorized", {
+  //       status: 403,
+  //     });
+  //   }
 
   console.log(await request.json());
 
