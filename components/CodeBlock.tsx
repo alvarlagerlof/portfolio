4e32c548258c832c4f59e8660b7691f8fbd1480c
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-08-27 14:49:17 +0200

    Remove unused @ts-ignore

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -2,7 +2,6 @@ import { Code } from "bright";
 
 export function CodeBlock({ language, code }: { language: string; code: string }) {
   return (
-    // @ts-ignore
     <Code lang={language} theme="github-dark" className="!rounded-2xl">
       {code.trim()}
     </Code>
