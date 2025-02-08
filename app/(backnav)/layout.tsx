6f19df5f7e136090d4c908b80e75ac8fbad2f617
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2025-02-08 21:08:06 +0100

    Upgrade to Tailwind v4

--- app/(backnav)/layout.tsx
+++ app/(backnav)/layout.tsx
@@ -6,7 +6,7 @@ export default function BackNavLayout({ children }) {
     <div className="flex flex-col items-center mx-4 sm:mx-8 md:mx-12">
       <div className="w-full xl:w-3/4 max-w-6xl">
         {/* <WithDividers direction="vertical"> */}
-        <nav className="!py-6">
+        <nav className="py-6!">
           <Link
             href="/blog"
             className="font-subheading text-xl text-primary font-semibold no-underline hover:underline"
--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -2,7 +2,7 @@ import { Code } from "bright";
 
 export function CodeBlock({ language, code }: { language: string; code: string }) {
   return (
-    <Code lang={language} theme="github-dark" className="!rounded-2xl">
+    <Code lang={language} theme="github-dark" className="rounded-2xl!">
       {code.trim()}
     </Code>
   );
